089b00c6bf53ae45701ce342519bc2adaf2cce60
sejinkang <gem763@hanmail.net>
2019-04-17 21:36:13 +0900

    .

--- backtester/views.py
+++ backtester/views.py
@@ -7,5 +7,9 @@ def dashboard(request):
     # return HttpResponse(123)
     bt = run_backtest()
     navs = bt.navs()
-    navs_strat = navs['Strategy'].values
-    return render(request, 'backtester/dashboard.html', {'results':navs_strat})
+    navs_strat = list(navs['Strategy'].values)
+    navs_bm = list(navs['BM'].values)
+    navs_min = max(round(navs.min().min(), 1) - 0.1, 0)
+    dates = list(navs.index.date.astype(str))
+    results = {'navs_strat':navs_strat, 'navs_bm':navs_bm, 'dates':dates, 'navs_min':navs_min}
+    return render(request, 'backtester/dashboard.html', {'results':results})
